f7db5de0eda17dad5c5254ff90af118b75e43e25
Shu Luo <luo.s@outlook.com>
2025-09-17 19:21:30 -0400

    p6 optimized

--- problem/p006_sum_square_difference.py
+++ problem/p006_sum_square_difference.py
@@ -22,6 +22,6 @@ def sumSquareDifference(n):
 
 # math
 def sumSquareDifference(n):
-    square_of_sum = pow(sum(range(1, n + 1)), 2)
+    square_of_sum = pow((n * (n + 1) / 2), 2)
     sum_of_square = n * (n + 1) * (2 * n + 1) / 6
     return  square_of_sum - sum_of_square 
