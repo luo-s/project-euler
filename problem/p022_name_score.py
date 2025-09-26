12af58f1878fc1fb3bc6a325c355f4e248c34814
Shu <luo.s@outlook.com>
2025-09-26 11:48:22 -0400

    p22 solved

---
+++ problem/p022_name_score.py
@@ -0,0 +1,16 @@
+# Using names, an array defined in the background containing over five-thousand first names, begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value by its alphabetical position in the list to obtain a name score.
+
+# For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
+
+# What is the total of all the name scores in the array?
+
+def name_score(arr):
+    arr.sort()
+    for i, name in enumerate(arr):
+        score = 0
+        for c in name:
+            score += ord(c.lower()) - ord('a') + 1
+        score *= (i + 1)
+        arr[i] = score
+    return sum(arr)
+
