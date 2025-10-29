3749d57d9d4ee46d75b8fff982c5e56999dd85e4
Shu <luo.s@outlook.com>
2025-10-29 14:20:21 -0400

    p49 solved

--- problem/p049_prime_permutations.py
+++ problem/p049_prime_permutations.py
@@ -4,5 +4,37 @@
 
 # What 12-digit number do you form by concatenating the three terms in this sequence?
 
+# brute force O(n^3)
+import math
 def prime_permutations():
-    
+    # all 4-digit primes
+    primes = [p for p in sieve_of_eratosthenes(10000) if 1000 <= p < 10000]
+    l = len(primes)
+    for a in range(0, l - 2):
+        for b in range(a + 1, l - 1):
+            for c in range(b + 1, l):
+                s1, s2, s3 = str(primes[a]), str(primes[b]), str(primes[c])
+                if sorted(s1) == sorted(s2) == sorted(s3) and primes[b] - primes[a] == primes[c] - primes[b] and s1 != '1487':
+                    return int(s1 + s2 + s3)
+
+
+
+def sieve_of_eratosthenes(n):
+    if n < 2:
+        return []
+
+    # Initialize all numbers as potential primes
+    is_prime = [True] * (n + 1)
+    is_prime[0] = is_prime[1] = False  # 0 and 1 are not primes
+
+    # Sieve process
+    for p in range(2, math.isqrt(n) + 1):
+        if is_prime[p]:
+            # Mark multiples of p as False (composite)
+            # start form p * p, since 2 * p, 3 * p, ... (p - 1) * p have already been marked when processing smaller primes
+            for multiple in range(p * p, n + 1, p):
+                is_prime[multiple] = False
+
+    # Extract all primes
+    primes = [i for i, prime in enumerate(is_prime) if prime]
+    return primes
